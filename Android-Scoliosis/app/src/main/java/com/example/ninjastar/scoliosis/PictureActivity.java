3c51642afbe90a936259b68601bcd44a0677f3b7
dtg <dtg@sfu.ca>
2015-04-09 16:46:08 -0700

    picture activity

---
+++ Android-Scoliosis/app/src/main/java/com/example/ninjastar/scoliosis/PictureActivity.java
@@ -0,0 +1,18 @@
+package com.example.ninjastar.scoliosis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by domz on 09/04/15.
+ */
+public class PictureActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+        
+    }
+}
--- Matlab-Scoliosis/startup.m
+++ Matlab-Scoliosis/startup.m
@@ -3,6 +3,7 @@ function startup()
 
 %% add jar file (Comment out the same line in ServerStart in connection)
 javaaddpath(fullfile(pwd, 'Connection', 'JavaConnection.jar'), '-end');
+clear java
 
 %% add Connection Scripts
 addpath(fullfile(pwd, 'Connection'));
